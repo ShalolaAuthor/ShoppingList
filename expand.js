621cbca4a89fc6efa03f84d101f2ca8d7a3ad984
ShalolaAuthor <124796924+ShalolaAuthor@users.noreply.github.com>
2024-05-23 06:14:24 +0200

    Delete expand.js

--- expand.js
+++
@@ -1,5 +0,0 @@
-const textbox = document.getElementById("userInput");
-textbox.addEventListener("input", function () {
-  textbox.style.height = "auto";
-  textbox.style.height = textbox.scrollHeight + "px";
-});
